7ba7f80623845eef494bd9c6fb34e8dfe49f6053
Douglas Vidal <vidaldl01@gmail.com>
2023-10-05 17:40:10 -0300

    finish

--- src/components/EditData.jsx
+++ src/components/EditData.jsx
@@ -6,19 +6,27 @@ import {
     MDBCol
 } from 'mdb-react-ui-kit';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 export default function EditData() {
     const { id } = useParams();
-    const [weatherData, setWeatherData] = useState(null)
+    const [weatherData, setWeatherData] = useState(null);
+    const [cidade, setCidade] = useState('');
+    const [datetime, setDatetime] = useState('');
+    const [weather, setWeather] = useState('');
+    const [icon, setIcon] = useState('');
+    const [temp, setTemp] = useState(0.0);
+    const [sensTerm, setSensTerm] = useState(0.0);
+    const [umid, setUmid] = useState(0.0);
 
+    const navigate = useNavigate();
 
     useEffect(() => {
         const fetchData = async () => {
-            const res = await axios.get(`http://localhost:4000/getWeatherById/${id}`)
+            const res = await axios.get(`http://localhost:4000/getWeatherById/${id}`);
 
-            // Converte para o horário local
+            // Convert to the local time
             res.data.datetime = new Date(res.data.datetime).toLocaleTimeString('pt-BR', {
                 day: '2-digit',
                 month: '2-digit',
@@ -28,11 +36,46 @@ export default function EditData() {
                 second: '2-digit'
             });
 
-            setWeatherData(res.data)
+            setWeatherData(res.data);
+            setCidade(res.data.cidade);
+            setDatetime(res.data.datetime);
+            setWeather(res.data.weather);
+            setIcon(res.data.icon);
+            setTemp(res.data.temp);
+            setSensTerm(res.data.sens_term);
+            setUmid(res.data.umid);
         }
+        fetchData();
+    }, [id]);
 
-        fetchData()
-    }, [])
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const dataISO = new Date(datetime).toISOString();
+            console.log(dataISO)
+            const updatedWeatherData = {
+                cidade,
+                datetime: dataISO,
+                weather,
+                icon,
+                temp,
+                sens_term: sensTerm,
+                umid,
+            };
+
+            const res = await axios.put(`http://localhost:4000/editWeatherData/${id}`, updatedWeatherData);
+
+            if (res.status === 200) {
+                alert("Dados editados com sucesso!");
+                navigate('/pesquisar');
+            } else {
+                alert("Erro ao editar dados:", res.data);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 
     return (
         <MDBContainer className='mt-5'>
@@ -42,35 +85,35 @@ export default function EditData() {
                     <form className='mt-3'>
                         <MDBRow>
                             <MDBCol>
-                                <MDBInput className='mb-4' type='text' id='cidade' label='Cidade' value={weatherData.cidade} />
+                                <MDBInput className='mb-4' type='text' id='cidade' name='cidade' label='Cidade' value={cidade} onChange={(e) => setCidade(e.target.value)} />
                             </MDBCol>
                             <MDBCol>
-                                <MDBInput className='mb-4' type='datetime' id='data' label='Data' value={weatherData.datetime} />
+                                <MDBInput className='mb-4' type='text' id='datetime' name='datetime' label='Data' value={datetime} onChange={(e) => setDatetime(e.target.value)} />
                             </MDBCol>
                         </MDBRow>
 
                         <MDBRow>
                             <MDBCol>
-                                <MDBInput className='mb-4' type='text' id='desc' label='Desc' value={weatherData.weather} />
+                                <MDBInput className='mb-4' type='text' id='weather' name='weather' label='Desc' value={weather} onChange={(e) => setWeather(e.target.value)} />
                             </MDBCol>
                             <MDBCol>
-                                <MDBInput className='mb-4' type='text' id='icon' label='Icon' value={weatherData.weather_icon} />
+                                <MDBInput className='mb-4' type='text' id='icon' name='icon' label='Icon' value={icon} onChange={(e) => setIcon(e.target.value)} />
                             </MDBCol>
                         </MDBRow>
 
                         <MDBRow>
                             <MDBCol>
-                                <MDBInput className='mb-4' type='text' id='temp' label='Temp' value={weatherData.temp} />
+                                <MDBInput className='mb-4' type='number' id='temp' name='temp' label='Temp' value={temp} step='0.1' onChange={(e) => setTemp(parseFloat(e.target.value))} />
                             </MDBCol>
                             <MDBCol>
-                                <MDBInput className='mb-4' type='text' id='sens' label='Sens.' value={weatherData.sens_term} />
+                                <MDBInput className='mb-4' type='number' id='sens_term' name='sens_term' label='Sens.' value={sensTerm} step='0.1' onChange={(e) => setSensTerm(parseFloat(e.target.value))} />
                             </MDBCol>
                             <MDBCol>
-                                <MDBInput className='mb-4' type='text' id='umid' label='Umid.' value={weatherData.umid} />
+                                <MDBInput className='mb-4' type='number' id='umid' name='umid' label='Umid.' value={umid} step='0.1' onChange={(e) => setUmid(parseFloat(e.target.value))} />
                             </MDBCol>
                         </MDBRow>
 
-                        <MDBBtn type='submit' className='mb-4' block>
+                        <MDBBtn onClick={handleSubmit} className='mb-4' block>
                             Editar
                         </MDBBtn>
                     </form>
@@ -80,5 +123,4 @@ export default function EditData() {
             )}
         </MDBContainer>
     );
-
 }
